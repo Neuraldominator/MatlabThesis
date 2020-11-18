887319b8043da8a383ccb1cf40df7a36a152786e
Dominik <dominik.kessler@uol.de>
2020-11-18 12:09:03 +0100

    Leave the empty cells in the preprocessed data and complete the function PreprocessingSpikes.m

--- Code_Tests/PreprocessingSpikes.m
+++ Code_Tests/PreprocessingSpikes.m
@@ -1,69 +1,166 @@
-function cleansed_spikes = PreprocessingSpikes(foldername, sr, cutoff)
+function [data, Rout] = PreprocessingSpikes(foldername, R_thresh, cutoff)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% deletes empty cells from the raw spike trains. 
+% Truncates the spike trains from those sessions of the raw spike data,
+% which have a higher driven spike rate than R_tresh. The truncation window
+% is given by [delay + cutoff, delay + durat].
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Input 
-%  foldername (str): struct with fields based on animal and cell type with
-%                    the recorded spike trains. 
-%  sr (double)     : minimal spike rate (threshold).
-%  cutoff (double) : onset cutoff time [ms] after stimulation.
+% Input: 
+%  foldername (str) : struct with fields based on animal and cell type with
+%                     the recorded spike trains. 
+%  R_thresh (double): spike rate threshold in [sp/sec], default is 30. All
+%                     sessions with a spike rate below R_thresh are removed
+%  cutoff (double)  : onset cutoff time [ms] after stimulation. Default is 
+%                     15.
 %
 % Output: 
-%  cleansed_spikes (struct): struct with fields based on animal and cell 
-%                            type with the cleansed spike trains.
+%  data (struct): struct with fields based on animal and cell type with the 
+%                 cleansed spike trains. Order of cell entries: spikes, 
+%                 depvar, freq, epoch, durat, delay and cutoff.
+%  Rout (struct): struct with fields representing the animal/ cell types.  
+%                 For each session (in cell) of a field, the driven spike 
+%                 rate is calculated from all non-spontaneous trials during
+%                 the time of stimulation. 
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % written by Dominik Kessler, Nov 2020 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-spike_data = RemoveEmptyCells(foldername);
+% default values
+if nargin < 2
+  R_thresh = 30;
+end
+
+spike_data = SpikeDataLoader(foldername);
 
 fields = fieldnames(spike_data);  % field names of spike_data struct
 n_folder = length(fields);  % number of folders
-cleansed_spikes = spike_data;  % init
+data = spike_data;  % init
+Rout = struct();  % init
 for folder = 1:n_folder
     % get all recordings from one animal and one cell type
-    sp = getfield(cleansed_spikes, fields(folder));  
+    sp = getfield(data, fields{folder});  
     
     % for each separate recording, access the spike trains
     n_sessions = length(sp);  % number of sessions
-    sp_session_cell = cell(n_sessions, 1);  % init.
-    depvar_session_cell = cell(n_sessions, 2);  % init.
-    for session = 1:n_sessions
-        % get all recordings of the current session
-        sp_session = sp{session}{1};  % spikes
-        depvar_session = sp{session}{2};  % depvar
+    session_cell = cell(1, 7);  %init.
+    session = 1;  % init. while loop
+    non_empty_session = 1; % init. counter
+    Rout_session = zeros(n_sessions, 1);  % init
+    while session <= n_sessions
+        % get the "session specifics"
+        sp_session = sp{session}{1};      % spikes of the current session
+        depvar_session = sp{session}{2};  % depvar of the current session
+        durat = sp{session}{5};           % duration of the current session
+        delay = sp{session}{6};           % delay of the current session
         
-        % remove empty trials (cells) from current session
-        sp_session(~cellfun('isempty', sp_session))  % spikes
-        depvar_session(~cellfun('isempty', sp_session))  % depvar
-
-        % for each trial of a session, do the preprocessing
-        n_trials = length(sp_session);  % number of trials   
-        trial_cell = cell(n_trials, 1);  % init.
-        for trial = 1:n_trials
-            sp_trial = sp_session{trial};
+        % get all non-spontaneous trials and update the depvar vector
+        sp_driven = sp_session(~isTrialSpontaneous(depvar_session));
+        dv_driven = depvar_session(~isTrialSpontaneous(depvar_session));
+        
+        % truncate the non-spontaneous trials
+        T1 = delay + cutoff;  % cut off the onset response 
+        T2 = delay + durat;  % end of stimulation time
+        SpikeTrainTruncWrapper = @(SPin) SpikeTrainTruncator(SPin, T1, T2);
+        trunc_sp = cellfun(SpikeTrainTruncWrapper, sp_driven, ...
+                           'UniformOutput', false);  
+        
+        % total number of spikes for truncated spike trains 
+        [Rout_session(session), ~, ~] = GetSpikeRate(trunc_sp, T2-T1); 
+        
+        
+        % apply the filtering
+        if Rout_session(session) >= R_thresh  
+            % storage
+            session_cell{non_empty_session, 1} = trunc_sp;  
+            session_cell{non_empty_session, 2} = dv_driven;
+            session_cell{non_empty_session, 3} = sp{session}{3}; 
+            session_cell{non_empty_session, 4} = sp{session}{4}; 
+            session_cell{non_empty_session, 5} = sp{session}{5};
+            session_cell{non_empty_session, 6} = sp{session}{6};
+            session_cell{non_empty_session, 7} = cutoff;
             
-            
-            if sp_trial / "recording time" < sr  % spike rate high enough
-                % delete the spike train and the corresponding depvar entry
-            
-            else  % truncate spike train to analysis window
-                t0 = Data.delay + cutoff;     % cut off the onset response 
-                t1 = Data.delay + Data.durat; % end of stimulation time 
-                SPT_no_onset{k} = temp((temp > t0) & (temp < t1)); %truncation
-            end
-            
-            trial_cell(trial) = sp_trial;  % update the changes
+            % increase counter
+            non_empty_session = non_empty_session + 1;
         end
         
-        sp_session_cell(session) = trial_cell;  % update the changes
-        depvar_session_cell(session) = "depvar";
-    end
-    
+        session = session + 1;
+        
+    end    
+
     % update all sessions for current folder
-    cleansed_spikes = setfield(cleansed_spikes, fields(folder), session_cell);
-    
+    data = setfield(data, fields{folder}, session_cell);
+    Rout = setfield(Rout, fields{folder}, Rout_session);
+
 end
 
 end % [eof]
+
+
+%--------------------------------------------------------------------------
+function B = isTrialSpontaneous(depvar)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Returns true if depvar is -6666 and false otherwise.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input: 
+%  depvar (double): stimulus parameter (-6666 indicates spontaneous 
+%                   response = no stimulus)
+%
+% Output: 
+%  B (boolean): true if trial is spontaneous; false otherwise.
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% written by Dominik Kessler, Nov 2020 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+B = depvar == -6666;
+
+end  %[eof]
+
+
+%--------------------------------------------------------------------------
+function SPout = SpikeTrainTruncator(SPin, T1, T2)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Truncate the spike train to the analysis window [T1, T2].
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input: 
+%  SPin (double): spike train [ms]
+%  T1 (double): start of truncation [ms]
+%  T2 (double): end of truncation [ms]
+%
+% Output: 
+%  SPout (double): truncates spike train [ms]
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% written by Dominik Kessler, Nov 2020 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+SPout = SPin((SPin >= T1) & (SPin <= T2)); 
+
+end  %[oef]
+
+
+%--------------------------------------------------------------------------
+function [R, Nsp, N] = GetSpikeRate(SPin, durat)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Truncate the spike train to the analysis window [T1, T2].
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input: 
+%  SPin (cell)   : cell array containing spike trains [ms].
+%  durat (double): duration of analysis [ms] 
+%
+% Output: 
+%  R (double)  : Spike Rate [sp/sec]
+%  Nsp (double): Number of total spikes.
+%  N (double)  : Number of trials/ spike trains
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% written by Dominik Kessler, Nov 2020 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% total number of spikes for truncated spike trains 
+Nsp = sum(cellfun(@numel, SPin)); 
+
+% total number of non-spontaneous trials of the session
+N = numel(SPin);
+
+% calculate the "driven spike rate" of the current session in [spikes/sec]
+R = 1000 * Nsp / (durat * N); 
+end  %[eof]
