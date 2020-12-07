e15e2c7cb305f29809355fcae182c92f9462b201
Dominik <dominik.kessler@uol.de>
2020-12-08 00:20:34 +0100

    Add script to create the VS-CI plot for the generated data.

---
+++ Gen_Data/genData_Analysis.m
@@ -0,0 +1,71 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This script calculates VS and CI for the artifical spike trains generated
+% in "generateData.m". Finally, the VS-CI curve is constructed.
+
+%% 1. Load the generated data
+load("Raw_GenData\gen_data.mat");
+
+%% 2. Add path of source code for VS and CI computation
+addpath("../Source_Code/Ashida_2020_code/invivo");
+% help calcPhaseHist
+% help calcSAC
+
+%% 3. Calculate VS and CIgen_data
+% number of cells for loop over
+N = size(gen_data.dec6_2020, 1);
+
+% set parameters for the "VS and CI computation" functions
+NB = 41;  % number of bins for phase histogram (irrelevant for VS calc.)
+BW = 0.05; % coincidence window [ms]
+
+% init.
+VS = zeros(N, 1);
+CI = zeros(N, 1);
+for k = 1:N
+  SPin = gen_data.dec6_2020{k, 1};  % spike train
+  FQ = gen_data.dec6_2020{k, 3};  % freq
+  
+  % start and end of analysis window
+  T1 = gen_data.dec6_2020{k, 6} + gen_data.dec6_2020{k, 7};  % delay + cutoff
+  T2 = gen_data.dec6_2020{k, 6} + gen_data.dec6_2020{k, 5};  % delay + durat
+    
+  [~, ~, VS(k)] = calcPhaseHist(SPin, T1, T2, NB, FQ);
+  
+  TL = gen_data.dec6_2020{k, 5}/5 * (1000/FQ);  % max binwidth (irrelevant for CI calc.)
+  [~, ~, CI(k), ~, ~] = calcSAC(SPin, BW, T1, T2, TL);
+end
+
+%% 4. Plot VS against CI
+figure
+plot(CI, VS, '+k')
+hold on
+
+% reference lines
+% plot
+hold off
+
+% plot cosmetics
+title("Relation between VS and CI based on generated data")
+xlabel("CI")
+ylabel("VS")
+ylim([0, 1])
+xlim([0, 10])
+
+%% compare VSin with empirical VS values 
+
+for k=1:N
+    VSin(k)=gen_data.dec6_2020{k, 10};
+end
+
+diff = VS - VSin';
+sqe = sum(diff.^2);
+
+% summary plot
+figure
+plot(VSin, VS, 'o')
+title("Compare VS (input vs. output)")
+ylabel("VSout")
+xlabel("VSin")
+xlim([0 1])
+ylim([0 1])
+
