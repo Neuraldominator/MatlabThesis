1cfffdf808f99349f00d38ebf06027ff74be147c
Dominik <dominik.kessler@uol.de>
2020-12-11 14:20:49 +0100

    Add theoretical curve in the VS-CI plot.

--- Analysis_Scripts/analysis_AN_GBC.m
+++ Analysis_Scripts/analysis_AN_GBC.m
@@ -2,7 +2,8 @@
 % Analysis script for AN (Auditory Nerve) and GBC (Globular Bushy Cell) 
 % data in the folders "..\Source_Code\ANmodel\ANdata" and 
 % "..\Source_Code\ANmodel\GBCdata". The folder ANdata is subdivided into 3
-% folders called "ANdata0", "ANdata1" and "ANdata2".
+% folders called "ANdata0", "ANdata1" and "ANdata2". The aim of this script
+% is to create the infamous VS-CI plot for this project.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% 1.1 Add data file paths (AN)
@@ -68,20 +69,29 @@ VSCI_AN70 = vertcat(VSCI_an0_70, VSCI_an1_70, VSCI_an2_70);
 VSCI_GBC40 = calcMetrics_genData(path_GBC, 40, "GBC", BW);
 VSCI_GBC70 = calcMetrics_genData(path_GBC, 70, "GBC", BW);
 
-%% 6. Plot the results
+%% 6. Sample the theoretical VS-CI relationship (for plot)
+kappa = 0.1:0.1:25;
+% Nk = length(kappa);
+VStheo = besseli(1,kappa) ./ besseli(0,kappa);
+CItheo = besseli(0,2*kappa) ./ (besseli(0,kappa)).^2;
 
-plot(VSCI_AN40(:,2), VSCI_AN40(:,1), 'b+')
+%% 7. Plot the results
+%load("VSCI_ANmodel.mat")
+figure
+plot(VSCI_AN40(:,2), VSCI_AN40(:,1), 'b+')  % AN 40db
 hold on
-plot(VSCI_AN70(:,2), VSCI_AN70(:,1), 'ro')
-plot(VSCI_GBC40(:,2), VSCI_GBC40(:,1), 'g*')
-plot(VSCI_GBC70(:,2), VSCI_GBC70(:,1), 'ks')
-hold off
+plot(VSCI_AN70(:,2), VSCI_AN70(:,1), 'ro')  % AN 70db
+plot(VSCI_GBC40(:,2), VSCI_GBC40(:,1), 'g*')  % GBC 40db
+plot(VSCI_GBC70(:,2), VSCI_GBC70(:,1), 'ys')  % GBC 70db
+plot(CItheo, VStheo, '-', 'LineWidth', 1.1, 'color', 'k')  % theo VS-CI
+
 title("VS-CI relationship for generated data (ANmodel)")
 xlabel("CI")
 ylabel("VS")
 xlim([0,10])
 ylim([0,1])
-legend("AN, 40db","AN, 70db","GBC, 40db","GBC, 70db", 'Location', 'best')
+legend("AN, 40db","AN, 70db","GBC, 40db","GBC, 70db", ...
+    "theoretical curve", 'Location', 'best')
 
 
 %% 7. Save the results
