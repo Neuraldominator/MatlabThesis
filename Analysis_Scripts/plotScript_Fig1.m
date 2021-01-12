85afd831adb06f625a1b77e6d3d1866a8344546a
Dominik <dominik.kessler@uol.de>
2021-01-13 00:28:10 +0100

    Initial commit.

--- Analysis_Scripts/plotScript_Fig1.m
+++ Analysis_Scripts/plotScript_Fig1.m
@@ -18,73 +18,160 @@ addpath("..\Source_Code\Ashida_2020_code\invivo")
 % set data path
 path_data = "../Raw_Data/gatorNL/";
 
-%% Raster Plot - good example
+% set cutoff
+cutoff = 15;  % in [ms]
+
+%% Some Preprocessing and Data Analysis - good example unit
 filename_good = "05.13.3.spikes.mat";
 data_good = load(path_data + filename_good); 
 spt_raw = data_good.spikes;
 depvar = data_good.depvar;
-Nrep = numel(spt_raw);  % number of repetitions
+durat = data_good.durat;  % stimulus duration
+delay = data_good.delay;  % start of stimulation
+Nrep_good = numel(spt_raw);  % number of repetitions
 
-% Removing Spike Doublets (note: the spike detection algorithm sometimes
-% detects two (or more) events for each spike -> only use the last one as
-% the correct spike
-trefract = 0.5;   % refractory window for rejecting double spikes
-spt = cell(Nrep, 1);  % init
-for n = 1:Nrep
- Nsp = length(spt_raw{n});     
- sptrev = fliplr(spt_raw{n});  
- sptdummy = [max(sptrev) + trefract, sptrev(1:Nsp-1)] ;
- sptrev = sptrev((sptdummy - sptrev) >= trefract);  % remove doublets
- spt{n} = fliplr(sptrev);
+% truncation window 
+T1_good = delay + cutoff;  % start of stimulation [ms]
+T2_good = delay + durat;  % end of stimulation [ms]
+
+% refractory window for rejecting double spikes
+trefract = 0.5;   
+
+spt_good = cell(Nrep_good, 1);  % init
+spt_trunc_good = cell(Nrep_good, 1);  % init
+for n = 1:Nrep_good
+   % Removing Spike Doublets 
+   Nsp = length(spt_raw{n});     
+   sptrev = fliplr(spt_raw{n});  
+   sptdummy = [max(sptrev) + trefract, sptrev(1:Nsp-1)] ;
+   sptrev = sptrev((sptdummy - sptrev) >= trefract);  % remove doublets (take last spike)
+   spt_good{n} = fliplr(sptrev);
+ 
+   % truncation 
+   temp = spt_good{n};
+   spt_trunc_good{n} = temp(temp >= T1_good & temp <= T2_good);
 end
 
-% plot
+% check if driven spike rate >= 30 and if Nsp >= 400 (after truncation)
+spt_trunc_stim_good = spt_trunc_good(depvar ~= -6666); 
+Nrep_stim_good = numel(spt_trunc_stim_good);
+Nsp_good = sum(cellfun(@numel, spt_trunc_stim_good));
+D_good = (T2_good - T1_good) / 1000;  % duration of truncation window [s]
+R_driven_good = Nsp_good / (D_good * Nrep_stim_good);
+if R_driven_good >= 30 && Nsp_good >= 400
+    sprintf("the unit %s fulfils the requirements.", filename_good)
+else
+    sprintf("the unit %s does not fulfil the requirements.", filename_good)
+end
+
+% remove all spontaneous trials
+spt_stim_good = spt_good(depvar ~= -6666);
+
+%% Raster Plot - good example unit
 figure
-for k = 1:Nrep
-    plot(spt{k}, k*ones(1,length(spt{k})) , '.k', 'MarkerSize', 5)
+for k = 1:numel(spt_stim_good)
+    plot(spt_stim_good{k}, k*ones(1,length(spt_stim_good{k})), '.k', ...
+        'MarkerSize', 5)
     hold on
 end
 hold off
 xlabel('time (ms)')
 ylabel('repetition')
 title(sprintf('Raster Plot - Gator NL %s', filename_good))
+xlim([0, data_good.epoch])
+ylim([0, numel(spt_stim_good) + 5])
 
-%% Raster Plot - bad example
+%% Some Preprocessing and Data Analysis - bad example unit
 filename_bad = "10.07.11.spikes.mat";
 data_bad = load(path_data + filename_bad); 
 spt_raw = data_bad.spikes;
 depvar = data_bad.depvar;
-Nrep = numel(spt_raw);  % number of repetitions
+durat = data_bad.durat;  % stimulus duration
+delay = data_bad.delay;  % start of stimulation
+Nrep_bad = numel(spt_raw);  % number of repetitions
 
-% removing doublets
-trefract = 0.5;   % refractory window for rejecting double spikes
-spt = cell(Nrep, 1);  % init
-for n = 1:Nrep
- Nsp = length(spt_raw{n});     
- sptrev = fliplr(spt_raw{n});  
- sptdummy = [max(sptrev) + trefract, sptrev(1:Nsp-1)] ;
- sptrev = sptrev((sptdummy - sptrev) >= trefract); 
- spt{n} = fliplr(sptrev);
+% truncation window
+T1_bad = delay + cutoff;  % start of stimulation [ms]
+T2_bad = delay + durat;  % end of stimulation [ms]
+
+% refractory window for rejecting double spikes
+trefract = 0.5;  % in [ms]
+
+spt_bad = cell(Nrep_bad, 1);  % init
+spt_trunc_bad = cell(Nrep_bad, 1);  % init
+for n = 1:Nrep_bad
+   % Removing Spike Doublets 
+   Nsp = length(spt_raw{n});     
+   sptrev = fliplr(spt_raw{n});  
+   sptdummy = [max(sptrev) + trefract, sptrev(1:Nsp-1)] ;
+   sptrev = sptrev((sptdummy - sptrev) >= trefract);  % remove doublets (take last spike)
+   spt_bad{n} = fliplr(sptrev);
+ 
+   % truncation 
+   temp = spt_bad{n};
+   spt_trunc_bad{n} = temp(temp >= T1_bad & temp <= T2_bad);
 end
 
-% plot
+% check if driven spike rate >= 30 and if Nsp >= 400 (after truncation)
+spt_trunc_stim_bad = spt_trunc_bad(depvar ~= -6666); 
+Nrep_stim_bad = numel(spt_trunc_stim_bad);
+Nsp_bad = sum(cellfun(@numel, spt_trunc_stim_bad));
+D_bad = (T2_bad - T1_bad) / 1000;  % duration of truncation window [s]
+R_driven_bad = Nsp_bad / (D_bad * Nrep_stim_bad);
+if R_driven_bad >= 30 && Nsp_bad >= 400
+    sprintf("the unit %s fulfils the requirements.", filename_bad)
+else
+    sprintf("the unit %s does not fulfil the requirements.", filename_bad)    
+end
+
+% remove all spontaneous trials
+spt_stim_bad = spt_bad(depvar ~= -6666);
+
+%% Raster Plot - bad example unit
 figure
-Nrep = numel(spt);
-for k = 1:Nrep
-    plot(spt{k}, k*ones(1,length(spt{k})) , '.k', 'MarkerSize', 5)
+for k = 1:numel(spt_stim_bad)
+    plot(spt_stim_bad{k}, k*ones(1,length(spt_stim_bad{k})), '.k', ...
+        'MarkerSize', 5)
     hold on
 end
 hold off
 xlabel('time (ms)')
 ylabel('repetition')
 title(sprintf('Raster Plot - Gator NL %s', filename_bad))
+xlim([0, data_bad.epoch])
+ylim([0, numel(spt_stim_bad) + 5])
 
 %% Period Histogram - good example
-%[PH, PHtv, VS] = calcPhaseHist(SPin, T1, T2, NB, FQ);
+NB = 41;
+FQ_good = data_good.freq;  % frequency [Hz]
+[PH_good, PHtv_good, VS_good] = calcPhaseHist(spt_trunc_good, T1_good, ...
+    T2_good, NB, FQ_good);
+
+% plot
+figure
+plot(PHtv_good, PH_good)
+title(sprintf("Phase Histogram - Gator NL %s, VS=%.2f, FQ=%d Hz", filename_good, VS_good, FQ_good))
+xlabel("phase (cycle)")
+ylabel("spike rate [Hz]")
 
 %% Period Histogram - bad example
+NB = 41;
+FQ_bad = data_bad.freq;  % frequency [Hz]
+[PH_bad, PHtv_bad, VS_bad] = calcPhaseHist(spt_trunc_bad, T1_bad, ...
+    T2_bad, NB, FQ_bad);
+
+% plot
+figure
+plot(PHtv_bad, PH_bad)
+title(sprintf("Phase Histogram - Gator NL %s, VS=%.2f, FQ=%d Hz", filename_bad, VS_bad, FQ_bad))
+xlabel("phase (cycle)")
+ylabel("spike rate [Hz]")
+
+%% SAC curve - good example
 
 
-%% SAC 
+%% SAC curve - bad example
+
+
 
 
