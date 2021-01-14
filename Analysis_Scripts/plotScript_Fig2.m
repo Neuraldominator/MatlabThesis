a564d19a0afab14f0d79ea45f5ad6edae185ea5d
Dominik <dominik.kessler@uol.de>
2021-01-14 16:03:57 +0100

    Add panel A,B,C,D,E,F.

--- Analysis_Scripts/plotScript_Fig2.m
+++ Analysis_Scripts/plotScript_Fig2.m
@@ -34,7 +34,6 @@ spt031 = gen_data.dec6_2020{idx031,1};
 spt061 = gen_data.dec6_2020{idx061,1};
 spt091 = gen_data.dec6_2020{idx091,1};
 
-% Plot the raster plots for these three simulated units
 % 1st raster plot VSin=0.31
 figure
 VSin031 = gen_data.dec6_2020{idx031, 10};
@@ -43,7 +42,7 @@ for k=1:400
    plot(spt031{k}, k*ones(1,length(spt031{k})), '.k', 'Markersize', 5)
    hold on
 end
-title(sprintf("Raster plot - simulated data VS=%.3f, Nsp=%d", VSin031, Nsp031))
+title(sprintf("Raster plot - simulated data VS=%.2f, Nsp=%.0f", VSin031, Nsp031))
 xlabel("time (ms)")
 ylabel("reps")
 xlim([-0.5, 155])
@@ -57,7 +56,7 @@ for k=1:400
    plot(spt061{k}, k*ones(1,length(spt061{k})), '.k', 'Markersize', 5)
    hold on
 end
-title(sprintf("Raster plot - simulated data VS=%.3f, Nsp=%d", VSin061, Nsp061))
+title(sprintf("Raster plot - simulated data VS=%.2f, Nsp=%.0f", VSin061, Nsp061))
 xlabel("time (ms)")
 ylabel("reps")
 xlim([-0.5, 155])
@@ -71,15 +70,53 @@ for k=1:400
    plot(spt091{k}, k*ones(1,length(spt091{k})), '.k', 'Markersize', 5)
    hold on
 end
-title(sprintf("Raster plot - simulated data VS=%.3f, Nsp=%d", VSin091, Nsp091))
+title(sprintf("Raster plot - simulated data VS=%.2f, Nsp=%.0f", VSin091, Nsp091))
 xlabel("time (ms)")
 ylabel("reps")
 xlim([-0.5, 155])
 ylim([0, 405])
 
 %% panel B: Phase histograms corresponding to the rasters in panel A
-% for the same 3 units as in Fig 2A, calculate the phase histogram using 
-% the function calcPhaseHist from Go’s Repo.
+% add path of source code provided by Go Ashida
+addpath("..\Source_Code\Ashida_2020_code\invivo\")
+
+% get some of the meta data (same for all 46 sweeps)
+durat = gen_data.dec6_2020{1,5};
+delay = gen_data.dec6_2020{1,6};
+cutoff = gen_data.dec6_2020{1,7};
+T1 = delay + cutoff; % start of analysis [ms] 
+T2 = delay + durat; % enf of analysis [ms]
+
+% set parameters
+NB = 41;  % number of bins used for the histogram
+FQ = gen_data.dec6_2020{1,3};  % stimulation frequency
+
+% phase histogram for VSin=0.31
+[PH031, PHtv031, VS031] = calcPhaseHist(spt031,T1,T2,NB,FQ);
+figure
+plot(PHtv031, PH031, '-k')
+title(sprintf("Phase Histogram - simulated data VSin=%.2f, VSout=%.2f",...
+    VSin031, VS031))
+xlabel("phase (cycle)")
+ylabel("spike rate (Hz)")
+
+% phase histogram for VSin=0.61
+[PH061, PHtv061, VS061] = calcPhaseHist(spt061,T1,T2,NB,FQ);
+figure
+plot(PHtv061, PH061, '-k')
+title(sprintf("Phase Histogram - simulated data VSin=%.2f, VSout=%.2f", ...
+    VSin061, VS061))
+xlabel("phase (cycle)")
+ylabel("spike rate (Hz)")
+
+% phase histogram for VSin=0.91
+[PH091, PHtv091, VS091] = calcPhaseHist(spt091,T1,T2,NB,FQ);
+figure
+plot(PHtv091, PH091, '-k')
+title(sprintf("Phase Histogram - simulated data VSin=%.2f, VSout=%.2f", ...
+    VSin091, VS091))
+xlabel("phase (cycle)")
+ylabel("spike rate (Hz)")
 
 %% panel C: VS-kappa curve
 kappa = 0:0.1:11;
@@ -91,7 +128,46 @@ ylabel('Vector Strength')
 title('VS-kappa curve')
 
 %% panel D: SAC curves corresponding to the rasters in panel A
+% add path of source code provided by Go Ashida
+addpath("..\Source_Code\Ashida_2020_code\invivo\")
 
+% get some of the meta data (same for all 46 sweeps)
+durat = gen_data.dec6_2020{1,5};
+delay = gen_data.dec6_2020{1,6};
+cutoff = gen_data.dec6_2020{1,7};
+
+% set parameters
+BW = 0.05; % coincidence window [ms]
+T1 = delay + cutoff; % start of analysis [ms] 
+T2 = delay + durat; % enf of analysis [ms]
+TL = 6; % max binwidth/delay shown in the SAC
+
+% phase histogram for VSin=0.31
+[SAC031, SACtv031, CI031, ~, Nsp031] = calcSAC(spt031, BW, T1, T2, TL);
+figure
+plot(SACtv031, SAC031, '-k')
+title(sprintf("SAC - simulated data VSin=%.2f, CI=%.2f, Nsp=%.0f", ...
+    VSin031, CI031, Nsp031))
+xlabel("delay (ms)")
+ylabel("norm. coincidences")
+
+% phase histogram for VSin=0.61
+[SAC061, SACtv061, CI061, ~, Nsp061] = calcSAC(spt061, BW, T1, T2, TL);
+figure
+plot(SACtv061, SAC061, '-k')
+title(sprintf("SAC - simulated data VSin=%.2f, CI=%.2f, Nsp=%.0f", ...
+    VSin061, CI061, Nsp061))
+xlabel("delay (ms)")
+ylabel("norm. coincidences")
+
+% phase histogram for VSin=0.91
+[SAC091, SACtv091, CI091, ~, Nsp091] = calcSAC(spt091, BW, T1, T2, TL);
+figure
+plot(SACtv091, SAC091, '-k')
+title(sprintf("SAC - simulated data VSin=%.2f, CI=%.2f, Nsp=%.0f", ...
+    VSin091, CI091, Nsp091))
+xlabel("delay (ms)")
+ylabel("norm. coincidences")
 
 %% panel E: CI-kappa curve
 kappa = 0:0.1:11;
