f4387e2d5e42fd1937671b2a03d611b7375dca95
Dominik <dominik.kessler@uol.de>
2021-01-18 17:46:26 +0100

    update documentation and range for theoretical curve.

--- Analysis_Scripts/ANmodel_VSCI_plot.m
+++ Analysis_Scripts/ANmodel_VSCI_plot.m
@@ -1,15 +1,15 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This script creates the VS-CI plot for the non-AM ANmodel data. The data
-% have been preprocessed in analysis_AN_GBC.m. Here, the VS and CI values
-% are plotted against each other (color coded for AN/GBC, 40db/70db).
-% Furthermore, the theoretical values are drawn into the same plot.
+% have been preprocessed in ANmodel_restructureData.m. Here, the VS and CI
+% values are plotted against each other (color coded for AN/ GBC, 40db/
+% 70db). Furthermore, the theoretical values are drawn into the same plot.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% 0. Load the data (further reference: analysis_AN_GBC.m) 
 load("VSCI_ANmodel.mat")
 
 %% 1. Sample the theoretical VS-CI relationship (for plot)
-kappa = 0.1:0.1:38;
+kappa = 0:0.1:40;
 % Nk = length(kappa);
 VStheo = besseli(1,kappa) ./ besseli(0,kappa);
 CItheo = besseli(0,2*kappa) ./ (besseli(0,kappa)).^2;
@@ -26,7 +26,7 @@ plot(CItheo, VStheo, '-', 'LineWidth', 1.1, 'color', 'k')  % theo VS-CI
 title("VS-CI relationship for generated data (ANmodel)")
 xlabel("CI")
 ylabel("VS")
-xlim([0,11])
-ylim([0,1])
+xlim([0 11])
+ylim([0 1])
 legend("AN, 40db","AN, 70db","GBC, 40db","GBC, 70db", ...
     "theoretical curve", 'Location', 'best')
