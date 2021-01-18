61e20b7ea17922df083e197fb088556bc6392267
Dominik <dominik.kessler@uol.de>
2021-01-18 14:09:16 +0100

    Initial commit.

---
+++ Analysis_Scripts/plotScript_Fig4.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This code produces the figures needed for Figure 4D-I of the paper, i.e.
+% raster plots, period histograms and SAC curves for two simulated units of
+% the ANmodel with skewed/bimodal spiking distributions, namely GBC 200Hz
+% 70db and AN 200Hz 40db.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% written by Dominik Kessler, Jan 2021
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
