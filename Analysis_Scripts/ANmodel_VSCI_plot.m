3992da79b8e96061af41f2df5533811fd0200a80
Dominik <dominik.kessler@uol.de>
2021-01-30 22:30:54 +0100

    Change the plot width and height.

--- Analysis_Scripts/ANmodel_VSCI_plot.m
+++ Analysis_Scripts/ANmodel_VSCI_plot.m
@@ -15,7 +15,7 @@ VStheo = besseli(1,kappa) ./ besseli(0,kappa);
 CItheo = besseli(0,2*kappa) ./ (besseli(0,kappa)).^2;
 
 %% 2. Plot the results
-figure
+f1 = figure;
 plot(VSCI_AN40(:,2), VSCI_AN40(:,1), 'b+')  % AN 40db
 hold on
 plot(VSCI_AN70(:,2), VSCI_AN70(:,1), 'ro')  % AN 70db
@@ -23,7 +23,7 @@ plot(VSCI_GBC40(:,2), VSCI_GBC40(:,1), 'g*')  % GBC 40db
 plot(VSCI_GBC70(:,2), VSCI_GBC70(:,1), 'ys')  % GBC 70db
 plot(CItheo, VStheo, '-', 'LineWidth', 1.1, 'color', 'k')  % theo VS-CI
 plot(CItheo*1.4, VStheo, '--', 'color', [0.4, 0.4, 0.4])  % CI = 1.4 * CI_est(VS)
-plot(CItheo*0.7, VStheo, '--', 'color', [0.6, 0.6, 0.6])  % CI = 0.7 * CI_est(VS)
+plot(CItheo*0.7, VStheo, '--', 'color', [0.4, 0.4, 0.4])  % CI = 0.7 * CI_est(VS)
 
 title("VS-CI relationship for generated data (ANmodel)")
 xlabel("CI")
@@ -32,3 +32,5 @@ xlim([0 11])
 ylim([0 1])
 legend("AN, 40db","AN, 70db","GBC, 40db","GBC, 70db", ...
     "theoretical curve", 'Location', 'best')
+set(f1,'Position',[360 198 726 350])
+
