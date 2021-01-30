45e7dfb6e1ef4436cd975c898a921cccb9637aec
Dominik <dominik.kessler@uol.de>
2021-01-30 23:14:12 +0100

    Initial commit.

---
+++ Analysis_Scripts/Fig2B_andFig4BEH_estimatedCIfromVS.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This code computes the estimated CI from 6 VS values. These values are
+% needed for Figure 2B and Figure 4B+E+H of the ARO poster.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+VS = [0.3, 0.62, 0.91, 0.34, 0.85, 0.74];
+
+%% Concentration parameter for von Mises distribution
+Ndata = length(VS);
+CItheo = zeros(Ndata, 1);
+for k = 1:Ndata
+    % go from VS to estimated CI
+    vsfun = @(K)(besseli(1,K) ./ besseli(0,K) - VS(k));
+    Kvs = fsolve(vsfun, 1.0, optimset('Display','off'));
+    CItheo(k) = besseli(0,2*Kvs) ./ (besseli(0,Kvs)^2);
+end
