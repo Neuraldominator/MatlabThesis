4ea88f0bac237be45980e4e397c7f3353905fa71
Dominik <dominik.kessler@uol.de>
2021-02-02 17:18:50 +0100

    Store the kappa values for each VS values too.

--- Analysis_Scripts/Fig2B_andFig4BEH_estimatedCIfromVS.m
+++ Analysis_Scripts/Fig2B_andFig4BEH_estimatedCIfromVS.m
@@ -3,14 +3,15 @@
 % needed for Figure 2B and Figure 4B+E+H of the ARO poster.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-VS = [0.3, 0.62, 0.91, 0.34, 0.85, 0.74];
+VS = [0.3, 0.62, 0.91, 0.34, 0.87, 0.74];
 
 %% Concentration parameter for von Mises distribution
 Ndata = length(VS);
+Kvs = zeros(Ndata, 1);
 CItheo = zeros(Ndata, 1);
 for k = 1:Ndata
     % go from VS to estimated CI
     vsfun = @(K)(besseli(1,K) ./ besseli(0,K) - VS(k));
-    Kvs = fsolve(vsfun, 1.0, optimset('Display','off'));
-    CItheo(k) = besseli(0,2*Kvs) ./ (besseli(0,Kvs)^2);
+    Kvs(k) = fsolve(vsfun, 1.0, optimset('Display','off'));
+    CItheo(k) = besseli(0,2*Kvs(k)) ./ (besseli(0,Kvs(k))^2);
 end
