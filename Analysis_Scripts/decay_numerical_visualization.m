05d1177011531e15c90d68dabbac8ba157d077ef
Dominik <dominik.kessler@uol.de>
2021-04-17 17:30:10 +0200

    Update.

--- Analysis_Scripts/decay_numerical_visualization.m
+++ Analysis_Scripts/decay_numerical_visualization.m
@@ -1,7 +1,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This is a demo of the decay of CI with respect to the bin width.
+% Depending on the concentration parameter kappa and the stimulus frequency
+% freq, just 5 summands seem enough to get a good accuracy.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% April 2021
+% written 8th April 2021 by Dominik Kessler
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 N     = [1, 2, 3, 4, 5];  % accuracy of the series
@@ -18,9 +20,11 @@ for n = 1:N(end)
         CI(n==N,:) = S;
     end
 end
+
+
 figure
 for n=1:5
-    plot(w,CI(n,:), '-o', 'color', rand(1,3))
+    plot(w,CI(n,:), '-o', 'color', round(rand(1,3),1))
     hold on
 end
 xlabel("bin width [ms]")
@@ -28,3 +32,16 @@ ylabel("CI")
 legend("N=1", "N=2", "N=3", "N=4", "N=5", "Location", "best")
 hold off
 
+
+%% plausibility check for series convergence
+target = besseli(0,2*kappa)/(besseli(0,kappa))^2;
+
+summe = 1;
+N = 20;
+for n=1:N
+    temp = 2 * (besseli(n,kappa) / besseli(0,kappa))^2;
+    summe = summe + temp;
+end
+
+diff = target - summe;
+% converges quite fast (in less than 20 steps)
